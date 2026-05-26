344b78da170852470822bd77dded6130c18be8ea
DV <62405752+MathieuDvv@users.noreply.github.com>
2026-05-26 12:33:02 +0200

    Disable sccache for test macOS build

--- .github/scripts/github_before_build.sh
+++ .github/scripts/github_before_build.sh
@@ -20,7 +20,6 @@ fi
 
 cp "$_main_repo/flags.gn" "$_src_dir/out/Default/args.gn"
 cat "$_root_dir/flags.macos.gn" >> "$_src_dir/out/Default/args.gn"
-echo 'cc_wrapper="sccache"' >> "$_src_dir/out/Default/args.gn"
 
 if ! [ -z "${PROD_MACOS_SPARKLE_ED_PUB_KEY-}" ]; then
   echo 'enable_sparkle=true' >> "$_src_dir/out/Default/args.gn"
